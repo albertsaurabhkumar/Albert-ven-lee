4845d42bad22e837d244cebcdddcae431871153a
SaurabhK <albertsaurabhkumar@gmail.com>
2023-11-23 15:56:16 +0530

    Added wdog and flash driver files

--- app/src/main.c
+++ app/src/main.c
@@ -5,18 +5,21 @@
 **  
 **  Main program file for the GCC Compiler
 */
+
+/*   PortPCR(PORTA,PIN11);
+  PortPCR(PORTB,PIN4);
+  DataDirectionGPIO(PTB,PIN4);
+  DataDirectionGPIO(PTA,PIN11); */
+
 #include "main.h"
 #include "clock.h"
+#include "port.h"
+#include "wdog.h"
 #include "common.h"
 
 volatile uint32_t dlyCnt;
 volatile uint32_t counter;
 //volatile uint32_t a=10;
-void WDOG_disable (void) {
- WDOG->CNT=0xD928C520; /*Unlock watchdog*/
- WDOG->TOVAL=0x0000FFFF; /*Maximum timeout value*/
- WDOG->CS = 0x00002100; /*Disable watchdog*/
-}
 
 void delay(uint32_t a) {
   while(a>0){
@@ -26,23 +29,16 @@ void delay(uint32_t a) {
 }
 
 int main(void) {
- initClk();
- WDOG_disable();
- PORTA->PCR[6] = 0x00000100;
- PORTA->PCR[11] = 0x00000100;
- PORTB->PCR[4] = 0x00000100;
+  initClk();
+  DisableWDOG();
 
-  PTA->PDDR = GPIO_PDDR_PDD(0x840);
-  PTB->PDDR = GPIO_PDDR_PDD(0x10);
+  PortPCR(PORTA,PIN6);
+  DataDirectionGPIO(PTA,PIN6);
 
   while(1)
   {
-    PTB->PSOR = GPIO_PSOR_PTSO(0x10);
-    delay(720000);
-    PTA->PTOR = GPIO_PTOR_PTTO(0x40);
-    delay(720000);
-    PTA->PTOR = GPIO_PTOR_PTTO(0x800);
-    delay(720000);
+    ToggleGPIO(PTA,PIN6);
+    delay(720);
     // Never returns from this loop 
   }
   return 0;
